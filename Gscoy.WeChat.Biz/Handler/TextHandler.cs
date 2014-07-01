21f6b4c51b5b966c1c68590260507e8ce7b5fb88
gaos@live.cn <gaos@live.cn>
2014-07-01 22:37:20 +0800

    5

---
+++ Gscoy.WeChat.Biz/Handler/TextHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gscoy.Common;
+using Gscoy.WeChat.Model.RequestModel;
+using Gscoy.WeChat.Model.ResonseModel;
+
+namespace Gscoy.WeChat.Biz.Handler
+{
+    public class TextHandler : IHandler
+    {
+        /// <summary>
+        /// 请求的XML
+        /// </summary>
+        private string RequestXml { get; set; }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="requestXml">请求的xml</param>
+        public TextHandler(string requestXml)
+        {
+            LogHelper.Trace(requestXml);
+            this.RequestXml = requestXml;
+        }
+        #region IHandler 成员
+
+        public string HandleRequest()
+        {
+            string response = string.Empty;
+            RequestText txt = RequestText.LoadFromXml(RequestXml);
+            string content = txt.Content.Trim();
+            if (string.IsNullOrEmpty(content))
+            {
+                response = "您什么都没输入，没法帮您啊，%>_<%。";
+            }
+            else
+            {
+                if (content.StartsWith("tq", StringComparison.OrdinalIgnoreCase))
+                {
+                    string cityName = content.Substring(2).Trim();
+                    response = cityName + "";
+                }
+                else
+                {
+                    response = "sfsfsfsfsd";
+                }
+            }
+            ResponseText rt = new ResponseText();
+            rt.Content = response;
+            rt.ToUserName = txt.FromUserName;
+            rt.FromUserName = txt.ToUserName;
+            rt.MsgType = txt.MsgType;
+            response = rt.ToXML();
+            return response;
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Gscoy.WeChat.Model/RequestModel/RequestText.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Gscoy.WeChat.Model.RequestModel
+{
+    public class RequestText : BaseRequestMsg
+    {
+        /// <summary>
+        ///  	消息id，64位整型 
+        /// </summary>
+        public int MsgID { get; set; }
+
+        /// <summary>
+        ///  	文本消息内容 
+        /// </summary>
+        public string Content { get; set; }
+
+        public static RequestText LoadFromXml(string requestXml)
+        {
+            RequestText txt = new RequestText();
+            var xml = XElement.Parse(requestXml);
+            txt.ToUserName = xml.Element("ToUserName").Value;
+            txt.FromUserName = xml.Element("FromUserName").Value;
+            txt.CreateTime = int.Parse(xml.Element("CreateTime").Value);
+            txt.Content = xml.Element("Content").Value;
+            txt.MsgID = int.Parse(xml.Element("").Value);
+            txt.MsgType = xml.Element("MsgType").Value;
+            return txt;
+        }
+    }
+}
