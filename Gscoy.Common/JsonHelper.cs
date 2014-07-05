9130196079034f278270b96e0dafc08763397dde
gaos@live.cn <gaos@live.cn>
2014-07-05 09:23:58 +0800

    8

--- Gscoy.Common/JsonHelper.cs
+++ Gscoy.Common/JsonHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using System.Data;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Gscoy.Common
 {
@@ -61,5 +64,76 @@ namespace Gscoy.Common
             }
             return default(T);
         }
+
+        #region DataTable 转换为Json 字符串
+        /// <summary>
+        /// DataTable 对象 转换为Json 字符串
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string ToJson(this DataTable dt)
+        {
+            ArrayList arrayList = new ArrayList();
+            foreach (DataRow dataRow in dt.Rows)
+            {
+                Dictionary<string, object> dictionary = new Dictionary<string, object>();  //实例化一个参数集合
+                foreach (DataColumn dataColumn in dt.Columns)
+                {
+                    dictionary.Add(dataColumn.ColumnName, dataRow[dataColumn.ColumnName].ToString());
+                }
+                arrayList.Add(dictionary); //ArrayList集合中添加键值
+            }
+
+            //return javaScriptSerializer.Serialize(arrayList);  //返回一个json字符串
+            return JsonConvert.SerializeObject(arrayList);
+        }
+        #endregion
+
+        #region Json 字符串 转换为 DataTable数据集合
+        /// <summary>
+        /// Json 字符串 转换为 DataTable数据集合
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable(this string json)
+        {
+            DataTable dataTable = new DataTable();  //实例化
+            DataTable result;
+            try
+            {
+                ArrayList arrayList = JsonConvert.DeserializeObject <ArrayList>(json);
+                if (arrayList.Count > 0)
+                {
+                    foreach (Dictionary<string, object> dictionary in arrayList)
+                    {
+                        if (dictionary.Keys.Count == 0)
+                        {
+                            result = dataTable;
+                            return result;
+                        }
+                        if (dataTable.Columns.Count == 0)
+                        {
+                            foreach (string current in dictionary.Keys)
+                            {
+                                dataTable.Columns.Add(current, dictionary[current].GetType());
+                            }
+                        }
+                        DataRow dataRow = dataTable.NewRow();
+                        foreach (string current in dictionary.Keys)
+                        {
+                            dataRow[current] = dictionary[current];
+                        }
+
+                        dataTable.Rows.Add(dataRow); //循环添加行到DataTable中
+                    }
+                }
+            }
+            catch
+            {
+            }
+            result = dataTable;
+            return result;
+        }
+        #endregion
     }
 }
---
+++ Gscoy.UnitTest/JsonHelperTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gscoy.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+namespace Gscoy.Common.Tests
+{
+    [TestClass()]
+    public class JsonHelperTests
+    {
+        [TestMethod()]
+        public void ToJsonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FromJsonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToJsonTest1()
+        {
+            var time = DateTime.Now.AddHours(1).ToString("yyyy-MM-dd HH:00:00");
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Age");
+            dt.Rows.Add(new[] { "a", "1" });
+            dt.Rows.Add(new[] { "b", "2" });
+            var json = JsonHelper.ToJson(dt);
+            var dd = JsonHelper.ToDataTable(json);
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToDataTableTest()
+        {
+            
+            Assert.Fail();
+        }
+    }
+}
